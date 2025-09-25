e1693242c96b6ebb43758b60dbcf21dbb8553112
Theo Birembaux <tbirembaux@iutbayonne.univ-pau.fr>
2025-09-25 10:16:26 +0200

    Ajout jassert, création de tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,12 @@
 package calculator;
 
 public class Calculator {
-
+    public int add(int opG, int opD)
+    {
+        return opG + opD;
+    }
+    public int divide(int opG, int opD)
+    {
+        return opG/opD;
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,7 +1,38 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CalculatorTest {
+    @Test
+    void add_devrait_calculer_la_somme_de_deux_entiers() {
+        //GIVEN
+        Calculator calculator = new Calculator();
+        int opG = 1;
+        int opD = 2;
+        int expected = 3;
 
+        //WHEN
+        int result = calculator.add(opG, opD);
+
+        //THEN
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void divide_devrait_calculer_le_quotient_de_deux_entiers() {
+        //GIVEN
+        Calculator calculator = new Calculator();
+        int opG = 4;
+        int opD = 2;
+        int expected = 2;
+
+        //WHEN
+        int result = calculator.divide(opG, opD);
+
+        //THEN
+        assertThat(result).isEqualTo(expected);
+    }
 }
